fdfe11866d98f611de5b7d4b319ae2a8605f959a
Alexis Leloup <alexleloup@yahoo.fr>
2021-11-29 17:52:37 +0100

    Add REST Professional support

--- src/main/java/com/taa/taa_spring_part3/rest/ProfessionalController.java
+++ src/main/java/com/taa/taa_spring_part3/rest/ProfessionalController.java
@@ -1,14 +1,16 @@
 package com.taa.taa_spring_part3.rest;
 
+import com.taa.taa_spring_part3.dao.AgendaDao;
 import com.taa.taa_spring_part3.dao.ProfessionalDao;
 import com.taa.taa_spring_part3.entities.Agenda;
 import com.taa.taa_spring_part3.entities.Professional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("professional")
@@ -17,27 +19,79 @@ public class ProfessionalController {
     @Autowired
     private ProfessionalDao professionalDao;
 
+    @Autowired
+    private AgendaDao agendaDao;
+
     /**
-     * GET /create  --> Create a new user and save it in the database.
+     * GET /create  --> Create a new professional with new agenda and save it in the database.
      */
-    @RequestMapping("/create")
+    @RequestMapping(value= "/createWithNewAgenda", params = { "firstName", "lastName", "login", "password", "agendaUrl", "agendaLogin", "agendaPassword"}, method = RequestMethod.GET)
     @ResponseBody
-    public String create(String firstName, String lastName, String login, String password, Agenda agenda) {
-        String userId = "";
+    public ResponseEntity createProfessionalWithAgenda(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName, @RequestParam("login") String login, @RequestParam("password") String password, @RequestParam("agendaUrl") String agendaUrl, @RequestParam("agendaLogin") String agendaLogin, @RequestParam("agendaPassword") String agendaPassword) {
         try {
+            Agenda agenda = new Agenda(agendaUrl, agendaLogin, agendaPassword);
+            agendaDao.save(agenda);
             Professional professional = new Professional(firstName, lastName, login, password, agenda);
             professionalDao.save(professional);
-            userId = String.valueOf(professional.getId());
+            return ResponseEntity.status(HttpStatus.CREATED).body(professional);
         }
         catch (Exception ex) {
-            return "Error creating the user: " + ex.toString();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
         }
-        return "Professional succesfully created with id = " + userId;
     }
 
-    @RequestMapping("/test")
+    /**
+     * GET /create  --> Create a new professional with new agenda and save it in the database.
+     */
+    @RequestMapping(value= "/createFromAgendaId", params = { "firstName", "lastName", "login", "password", "agendaId"}, method = RequestMethod.GET)
     @ResponseBody
-    public String home(){
-        return "Status : OK";
+    public ResponseEntity createProfessionalWithAgendaId(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName, @RequestParam("login") String login, @RequestParam("password") String password, @RequestParam("agendaId") Long agendaId) {
+        try {
+            Optional<Agenda> agenda = agendaDao.findById(agendaId);
+            if (agenda.isPresent()){
+                Professional professional = new Professional(firstName, lastName, login, password, agenda.get());
+                professionalDao.save(professional);
+                return ResponseEntity.status(HttpStatus.CREATED).body(professional);
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error, agenda with id=" + agendaId + " not found");
+            }
+        }
+        catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+        }
+    }
+
+    @RequestMapping("/all")
+    @ResponseBody
+    public ResponseEntity getAllProfessionals(){
+        try {
+            return ResponseEntity.status(HttpStatus.FOUND).body(professionalDao.findAll());
+        }
+        catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+        }
+    }
+    @RequestMapping(value= "/get", params = { "id"})
+    @ResponseBody
+    public ResponseEntity getProfessionalFromId(@RequestParam("id") Long id){
+        try {
+            Optional<Professional> pro = professionalDao.findById(id);
+            if (pro.isPresent()){
+                return ResponseEntity.status(HttpStatus.FOUND).body(pro.get());
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professional with Id=" + id + " not found.");
+            }
+        }
+        catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+        }
+    }
+
+    @RequestMapping("/state")
+    @ResponseBody
+    public ResponseEntity test(){
+        return ResponseEntity.status(HttpStatus.OK).body("State : OK");
     }
 }
